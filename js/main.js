79afba30bc765ae6259c8deb7330810a88c33d2d
Anshuli Gupta <anshuli30@gmail.com>
2015-12-16 18:01:03 +0530

    blog page

---
+++ js/main.js
@@ -0,0 +1,41 @@
+
+$(function() {
+
+    // Populate using an object literal
+
+    // WTF.init({
+
+    //     heading: [
+    //         "It's a *ing",
+    //         "Check this shit out, a *ing"
+    //     ],
+    //     response: [
+    //         "Already *ing seen one",
+    //         "Give me a-*ing-nother one",
+    //         "Don't really give a F"
+    //     ],
+    //     template: [
+    //         "Big @color @animal",
+    //         "Silly @animal with @color fur"
+    //     ],
+    //     animal: [
+    //         "dog",
+    //         "cat",
+    //         "rabbit"
+    //     ],
+    //     color: [
+    //         "red",
+    //         "blue",
+    //         "green",
+    //         "yellow"
+    //     ]
+    // });
+
+    // Populate using a JSON file
+    // WTF.init( 'sample.json' );
+
+    // Populate using a Google spreadsheet ID (you must publish it first!)
+    // @see https://support.google.com/drive/answer/37579?hl=en
+    WTF.init( '0AvG1Hx204EyydF9ub1M2cVJ3Z1VGdDhTSWg0ZV9LNGc' );
+
+});
